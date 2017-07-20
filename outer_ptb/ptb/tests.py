8a908b300efb6f35e222657e023eb0fc59cba7a8
Jack <92jackhenderson@gmail.com>
2017-07-20 16:18:05 +0100

    Added homepage
    - Write test for homepage status code
    - Write test for homepage correct template
    - Write test for homepage page content

--- outer_ptb/ptb/tests.py
+++ outer_ptb/ptb/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class PollsViewsTestCase(TestCase):
+    def test_index(self):
+        response = self.client.get('/ptb/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ptb/home.html')
+        self.assertContains(response, '<h1> Pass The Booking </h1>')
--- outer_ptb/ptb/views.py
+++ outer_ptb/ptb/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Client
+from .models import Property
+from .models import Booking
 # Create your views here.
+
+def home(request):
+    return render(request, 'ptb/home.html', {} )
